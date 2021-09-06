e9f21cebb49787ebc0d096406a7972df2a863713
Arnaldo Aparicio <apaarn2211@gmail.com>
2021-09-05 22:52:11 -0400

    Initial commit

---
+++ section1/ex1.rb
@@ -0,0 +1,11 @@
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts "Yay! Printing."
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
+
+ # Using a hash, or octothorpe will omit a line of code. By doing so, the line will not run.
+
+# puts "This is another line"
