7251bd14c1b96fa24421bfaf01e084f0ecb8dd27
Arnaldo Aparicio <apaarn2211@gmail.com>
2021-09-11 21:07:49 -0400

    Add Final Prep work

--- final_prep/mod_zero_hero.rb
+++ final_prep/mod_zero_hero.rb
@@ -2,39 +2,79 @@
 
 # Declare two variables - hero_name AND special_ability - set to strings
 
+hero_name = "Captain Phantasm"
+special_ability = "persuasion"
+
 # Declare two variables - greeting AND catchphrase
 #   greeting should be assigned to a string that uses interpolation to include the hero_name
 #   catchphrase should be assigned to a string that uses interpolation to include the special_ability
 
+greeting = "Good day citizen! I am #{hero_name}!"
+catchphrase = "With the power of #{special_ability}, anything is possible!"
+
+
 # Declare two variables - power AND energy - set to integers
+
+power = 1000
+energy = 80
 
 # Declare two variables - full_power AND full_energy
 #   full_power should multiply your current power by 500
 #   full_energy should add 150 to your current energy
 
+full_power = power * 500
+full_energy = energy + 150
+
 # Declare two variables - is_human and identity_concealed - assigned to booleans
 
+is_human = false
+identity_concealed = true
 
 # Declare two variables - arch_enemies AND sidekicks
 #   arch_enemies should be an array of at least 3 different enemy strings
 #   sidekicks should be an array of at least 3 different sidekick strings
 
+arch_nemesis = ["The Destoyer", "Dr. Schultz", "King Crimson"]
+sidekick = ["Tin Boy", "Ice Girl", "Mr. Danger"]
+
 # Print the first sidekick to your terminal
+p sidekick[0]
 
 # Print the last arch_enemy to the terminal
 
+p arch_nemesis[2]
 # Write some code to add a new arch_enemy to the arch_enemies array
+
+arch_nemesis << "DIO"
 
 # Print the arch_enemies array to terminal to ensure you added a new arch_enemey
 
+p arch_nemesis
 # Remove the first sidekick from the sidekicks array
 
+sidekick.delete_at(0)
+
 # Print the sidekicks array to terminal to ensure you added a new sidekick
+p sidekick
 
 # Create a function called assess_situation that takes three arguments - danger_level, save_the_day, bad_excuse
 #   - danger_level should be an integer
-#   - save_the_day should be a string a hero would say once they save the day 
+#   - save_the_day should be a string a hero would say once they save the day
 #   - bad_excuse should be a string a hero would say if they are too afraid of the danger_level
+
+def assess_situation(danger_level, save_the_day, bad_excuse)
+  @danger_level = danger_level
+  @save_the_day = save_the_day
+  @bad_excuse = bad_excuse
+
+  if danger_level > 50
+    puts bad_excuse
+  elsif danger_level.between?(10,50)
+    puts save_the_day
+  else
+    puts "Meh. Hard pass."
+ end
+end
 
 # Your function should include an if/else statement that meets the following criteria
 #   - Danger levels that are above 50 are too scary for your hero. Any danger level that is above 50 should result in printing the bad_excuse to the terminal
@@ -44,9 +84,11 @@
 #Test Cases
 announcement = 'Never fear, the Courageous Curly Bracket is here!'
 excuse = 'I think I forgot to lock up my 1992 Toyota Coralla. Be right back.'
-# assess_situation(99, announcement, excuse) > Should print - 'I think I forgot to lock up my 1992 Toyota Coralla. Be right back.'
-#assess_situation(21, announcement, excuse) > should print - 'Never fear, the Courageous Curly Bracket is here!'
-#assess_situation(3, announcement, excuse) > should print - "Meh. Hard pass."
+ assess_situation(99, announcement, excuse) #> Should print - 'I think I forgot to lock up my 1992 Toyota Coralla. Be right back.'
+assess_situation(21, announcement, excuse) #> should print - 'Never fear, the Courageous Curly Bracket is here!'
+assess_situation(3, announcement, excuse) #> should print - "Meh. Hard pass."
+
+
 
 # Declare a new variable - scary_monster - assigned to an hash with the following key/values
 #   - name (string)
@@ -56,29 +98,78 @@ excuse = 'I think I forgot to lock up my 1992 Toyota Coralla. Be right back.'
 #   - luckyNumbers (array)
 #   - address (hash with following key/values: number , street , state, zip)
 
+scary_monster = { "name" => "Lutheron",
+                 "smell" => "sulfur",
+                 "weight" => 1000,
+                 "citiesDestroyed" => ["Detroit", "Los Angeles", "Tokyo"],
+                 "luckNumbers" => [3, 65, 21, 19],
+                 "address" => {"number" => 25, "street" => "Johnson Ave",
+                               "state" => "CA", "zip" => 90210}
+
+}
 
 # Create a new class called SuperHero
 # - Your class should have the following DYNAMIC values
-#   - name 
+#   - name
 #   - super_power
-#   - age 
+#   - age
 # - Your class should have the following STATIC values
 #   - arch_nemesis, assigned to "The Syntax Error"
 #   - power_level = 100
-#   - energy_level = 50 
+#   - energy_level = 50
 
 # - Create the following class methods
 #   - say_name, should print the hero's name to the terminal
 #   - maximize_energy, should update the energy_level to 1000
 #   - gain_power, should take an argument of a number and INCREASE the power_level by that number
 
-# - Create 2 instances of your SuperHero class
+class SuperHero
+  attr_accessor :name, :super_power, :age
+  def initialize(name, super_power, age)
+   @name = name
+   @super_power = super_power
+   @age = age
+   @arch_nemesis = "The Syntax Error"
+   @power_level = 100
+   @energy_level = 50
+ end
 
+  def say_name
+    puts name
+  end
+
+  def maximize_energy
+    energy_level = 1000
+  end
+
+  def gain_power(number)
+     @power_level + number
+  end
+end
+
+
+# - Create 2 instances of your SuperHero class
+savior = SuperHero.new("Savior", "Laser Eyes", 19)
+
+  savior.say_name
+p savior.maximize_energy
+p savior.gain_power(50)
+
+
+wally_girl = SuperHero.new("Wally Girl", "Super Strength", 24)
+wally_girl.say_name
+p wally_girl.maximize_energy
+p wally_girl.gain_power(70)
 
 # Reflection
 # What parts were most difficult about this exerise?
 
+# Most difficult part is creating a class and everything within it. Different
+#  syntax errors kept popping up so I had to constantly fix it until it ran.
+
 # What parts felt most comfortable to you?
+# I felt comfortable creating the actual methods. Making a method
+# that allows it to do an action is very fun.
 
 # What skills do you need to continue to practice before starting Mod 1?
-
+# I need to continue making classes, typing hashes, and do more iteration.
